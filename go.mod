0504874ca10d001efedcb0a5054900e0e693b609
Brian Dwyer <Brian.Dwyer@broadridge.com>
2019-11-21 16:27:38 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/bdwyertech/aws-get-secret
+
+go 1.13
+
+require (
+	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
+	github.com/aws/aws-sdk-go v1.25.39
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mkideal/cli v0.0.3
+	github.com/mkideal/pkg v0.0.0-20170503154153-3e188c9e7ecc // indirect
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+
+	"github.com/mkideal/cli"
+)
+
+var GitCommit string
+var ReleaseVer string
+
+func showVersion() {
+	if GitCommit == "" {
+		GitCommit = "DEVELOPMENT"
+	}
+	if ReleaseVer == "" {
+		ReleaseVer = "DEVELOPMENT"
+	}
+	fmt.Printf("version: %s\ncommit: %s", ReleaseVer, GitCommit)
+}
+
+type ArgT struct {
+	cli.Helper
+	Parameter       string `cli:"p,parameter" usage:"The path to the parameter in AWS Parameter Store"`
+	Profile         string `cli:"profile" usage:"AWS Profile" dft:"$AWS_PROFILE"`
+	Region          string `cli:"region" usage:"AWS Region" dft:"$AWS_REGION"`
+	RoleARN         string `cli:"r,role-arn" usage:"The Role ARN to assume to retrieve the parameter"`
+	RoleSessionName string `cli:"session-name" usage:"The Session Name to use during the AssumeRole operation"`
+	ExternalID      string `cli:"e,external-id" usage"The External ID to use during the AssumeRole operation"`
+	Version         bool   `cli:"v, version" usage:"Display the version of this utility"`
+}
+
+// Validate & Coalesce CLI Arguments
+func (argv *ArgT) Validate(ctx *cli.Context) (err error) {
+
+	if argv.Version {
+		showVersion()
+		os.Exit(0)
+	}
+
+	if argv.Parameter == "" {
+		if len(ctx.Args()) > 0 {
+			argv.Parameter = ctx.Args()[0]
+		} else {
+			err = fmt.Errorf("%s is a required argument!", ctx.Color().Yellow("Parameter"))
+			return
+		}
+	}
+
+	if argv.Region == "" {
+		argv.Region = "us-east-1"
+	}
+
+	return
+}
+
+func main() {
+	os.Exit(cli.Run(new(ArgT), func(ctx *cli.Context) (err error) {
+		err = Run(ctx)
+		return
+	}))
+}
+
+func Run(ctx *cli.Context) (err error) {
+	argv := ctx.Argv().(*ArgT)
+	// Marshal Parameter
+	prm := argv.Parameter
+	region := argv.Region
+	switch len(strings.Split(prm, ":")) {
+	case 6:
+		prm_arn, err := arn.Parse(prm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		prm = strings.Split(prm, ":parameter")[1]
+		if prm_arn.Region != "" {
+			region = prm_arn.Region
+		}
+		resource := strings.Split(prm_arn.Resource, "parameter")
+		if len(resource) == 2 {
+			prm = resource[1]
+		} else {
+			fmt.Println(resource)
+			log.Fatal("ERROR: Bad Parameter Resource Path")
+		}
+	case 1:
+		break
+	default:
+		log.Fatal("ERROR: Invalid Parameter ARN or Path!")
+	}
+
+	// AWS Session
+	sess_opts := session.Options{
+		Config: *aws.NewConfig().WithRegion(region),
+	}
+	if argv.Profile != "" {
+		sess_opts.Profile = argv.Profile
+	}
+	sess := session.Must(session.NewSessionWithOptions(sess_opts))
+
+	creds := &credentials.Credentials{}
+	if argv.RoleARN != "" {
+		// Get AssumeRole Credentials
+		creds = stscreds.NewCredentials(sess, argv.RoleARN, func(p *stscreds.AssumeRoleProvider) {
+			if argv.RoleSessionName != "" {
+				p.RoleSessionName = argv.RoleSessionName
+			} else {
+				p.RoleSessionName = fmt.Sprintf("aws-get-secret-%d", time.Now().UTC().UnixNano())
+			}
+			if argv.ExternalID != "" {
+				p.ExternalID = aws.String(argv.ExternalID)
+			}
+		})
+	} else {
+		creds = sess.Config.Credentials
+	}
+
+	// SSM Client
+	ssmcfg := &aws.Config{
+		Credentials: creds,
+	}
+	ssmclient := ssm.New(sess, ssmcfg)
+	resp, err := ssmclient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(prm),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		log.Fatalf("ERROR: ssm.GetParameter:: %s\n%s", prm, err)
+	}
+	val := *resp.Parameter.Value
+
+	fmt.Println(val)
+
+	return
+}
